3637f6456aa3ae834be14c6335f8411c521074b6
Yaniel <yaniel2015@yandex.com>
2021-07-21 08:36:10 -0300

    Update

--- components/molecules/ArticleContent.js
+++ components/molecules/ArticleContent.js
@@ -16,7 +16,7 @@ const ArticleContent = ({ article }) => {
             <div className="mb-2 md:hidden"><DateLine date={publishDate} /></div>
             <Tags tags={article.contentfulMetadata.tags} />
             <div className="mb-5 rounded border">
-                <Image src={article.cover.url} width={800} height={800} alt={article.cover.title} />
+                {article.cover &&<Image src={article.cover.url} width={800} height={800} alt={article.cover.title} />}
             </div>
             <div className="text-justify">{!article.content ? article.description : documentToReactComponents(article.content.json)}</div>
         </div>
--- components/molecules/ListTech.js
+++ components/molecules/ListTech.js
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types'
 import IconDev from '../atoms/IconDev'
+import ReactTooltip from 'react-tooltip';
 
 const ListTech = () => {
-    const tech = ['devicon-react-original', 'devicon-laravel-plain', 'devicon-nodejs-plain', 'devicon-flutter-plain', 'devicon-cplusplus-plain', 'devicon-java-plain']
+    const tech = [
+        {name: 'React JS', icon: 'devicon-react-original'}, 
+        {name: 'Laravel', icon: 'devicon-laravel-plain'}, 
+        {name: 'Node JS', icon: 'devicon-nodejs-plain'},
+        {name: 'Flutter', icon: 'devicon-flutter-plain'}, 
+        {name: 'C++', icon: 'devicon-cplusplus-plain'},
+        {name: 'Java', icon: 'devicon-java-plain'}
+    ]
 
     return (
-        <div className="mt-8 flex space-y-2 w-full pb-5 justify-center flex-wrap">
-            {tech.map((item, index) => <IconDev key={index} name={item} />)}
-        </div>
+        <>
+            <div className="mt-8 flex space-y-2 w-full pb-5 justify-center flex-wrap">
+                {tech.map((item, index) => <div className="w-3/12 md:w-1/12 flex justify-center" data-background-color="#DD0837" type="dark" data-tip={item.name}>
+                    <IconDev key={index} name={item.icon} />
+                </div>)}
+            </div>
+            <ReactTooltip/>
+        </>
     )
 }
 
